4a8d54d8690545a580d8c82932d3ea5decb4bc3f
Erdeuz <erdeuz01@gmail.com>
2025-01-16 00:35:16 -0300

    bueno, ya hice una prueba un poco mas larga

--- primeros_pasos_git.py
+++ primeros_pasos_git.py
@@ -1,5 +1,11 @@
-
-print("hola pedazo de GIT")
-print("que onda pedazo de pancho?")
-print("esta es la version 1.01 donde agrego tambien:")
-print("que saltas pedazo de salchicha")
+print("este programita es una prueba para realizar pruebas de estructura de versionado")
+n1 = 1
+while n1 <= 20:
+    print("para que una teta sea considerada grande, debe estar en los siguientes tamaños:", (n1 + 94))
+    n1 = n1 + 1
+    if (n1 + 94) > 104:
+        print("no puede existir una teta tan grande!!!!!")
+        n1 = n1 + 199
+print("fin del loop, anduvo todo re copado")
+print("hola mica, ya te voy a agarrar el viernes ;)")
+print("preparate ^.^")
